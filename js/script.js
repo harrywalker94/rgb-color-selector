bec27025a374238e81ae3e079f89175676296127
harry walker <harrywalker312@gmail.com>
2020-03-17 09:48:58 +0000

    adding styling

--- js/script.js
+++ js/script.js
@@ -1,43 +1,29 @@
 $(document).ready(function () {
+  //will display rgb value of selected panel in the reset panel
+  $(".theButton").mouseenter(function () {
+    var colorText = $(this).css("background-color");
+    $(".superButton").text(colorText);
+  });
 
-    //will display rgb value of selected panel in the reset panel
-    $(".theButton").mouseenter(function () {
-        var colorText = $(this).css('background-color');
-        $('.superButton').text(colorText);
+  $("#btn").click(function () {
+    // Single line Reset function executes on click of Reset Button
+    $(".superButton").text("");
+  });
 
-    });
+  /* Button for copying the RGB Color */
+  $(".theButton").click(function () {
+    // Store the text you want to copy in variable
+    var text = $(this).css("background-color");
 
-     $(".theButton").mouseleave(function () {
-       $('.superButton').text("");
+    // Move the text to input tag to execute copy command
+    var tempElement = $("<input>")
+      .val(text)
+      .appendTo("body")
+      .select();
+    document.execCommand("copy");
+    tempElement.remove();
 
-   }); 
-
-   /* $("#btn").click(function () {
-        // Single line Reset function executes on click of Reset Button 
-        $(".superButton").text('');
-    }); */ // Commented out, add if required
-
-    $(".theButton").click(function () {
-        // Store the text you want to copy in variable
-        var text = $(this).css('background-color');
-
-        // Move the text to input tag to execute copy command
-        var tempElement = $('<input>').val(text).appendTo('body').select();
-        document.execCommand('copy');
-        tempElement.remove();
-
-        alert('RGB Colour Copied');
-
-    });
-
-
-
-    
+    alert("RGB Colour Copied");
+  });
 
 });
-
-
-
-
-
-
